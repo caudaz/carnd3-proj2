42e83e72056ce680de1d371a91dd140e11c697fe
root <root@fx8350.localdomain>
2017-11-15 17:43:20 -0600

    changes on optimize

--- main.py
+++ main.py
@@ -94,11 +94,14 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     """
     # TODO: Implement function
     
-    logits = tf.reshape(nn_last_layer, (-1, num_classes))
-    
+    logits        = tf.reshape(nn_last_layer, (-1, num_classes))
+	correct_label = tf.reshape(correct_label, (-1, num_classes))
+	
     cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=correct_label))
     
-    train_op = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy_loss)
+    optimizer_adam = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy_loss)
+	
+	train_op = optimizer_adam.minimize(cross_entropy_loss)
     
     return logits, train_op, cross_entropy_loss
 
